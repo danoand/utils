2d7afd6f8a1238029900dcc5bea21a75364d9b0b
danoand <danandersonmobile@yahoo.com>
2015-01-25 12:35:08 -0600

    Rename method

--- utils.go
+++ utils.go
@@ -6,7 +6,7 @@ import (
 )
 
 // Function to write JSON to an HTTP response
-func JSON(w http.ResponseWriter, val interface{}) {
+func ToJSON(w http.ResponseWriter, val interface{}) {
     w.Header().Set("Content-Type", "application/json")
     b, _ := json.Marshal(val)
     w.Write(b)
