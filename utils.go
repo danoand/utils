2ebaaf123e194a69e0731e56c98749c4accc60e6
danoand <danandersonmobile@yahoo.com>
2015-01-25 12:28:44 -0600

    Add a comment

--- utils.go
+++ utils.go
@@ -5,6 +5,7 @@ import (
     "net/http"
 )
 
+// Function to write JSON to an HTTP response
 func JSON(w http.ResponseWriter, val interface{}) {
     w.Header().Set("Content-Type", "application/json")
     b, _ := json.Marshal(val)
