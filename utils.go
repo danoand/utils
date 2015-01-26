63f8a3cbbf23d234ac64d706c88c045d7ac466f3
danoand <danandersonmobile@yahoo.com>
2015-01-26 10:19:36 -0600

    Update utils.go

--- utils.go
+++ utils.go
@@ -6,7 +6,7 @@ import (
 )
 
 // Function to write JSON to an HTTP response
-func ToJSON(w http.ResponseWriter, val interface{}) error {
+func ToJSONResponse(w http.ResponseWriter, val interface{}) error {
     w.Header().Set("Content-Type", "application/json")
     b, berr := json.Marshal(val)
     w.Write(b)
